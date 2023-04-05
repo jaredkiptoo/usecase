854dda3b8b664c3c1ab8c18febbf1801207cdc2b
jaredkiptoo <jaredkiptoo66@gmail.com>
2023-04-05 09:24:25 +0300

    first commit

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,7 +1,5 @@
 from abc import ABC
-
 from car import Car
-
 
 class CapuletEngine(Car, ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
@@ -9,5 +7,6 @@ class CapuletEngine(Car, ABC):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 30000
+    def engine_should_be_serviced(self) -> bool:
+        MILEAGE_THRESHOLD = 30000
+        return self.current_mileage - self.last_service_mileage > MILEAGE_THRESHOLD
